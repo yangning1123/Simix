d544abe6b464d3062cfd351466f3d1bcb5e73bb2
yangning <yangning1123@gmail.com>
2015-08-15 23:25:38 +0800

    定义从软盘读取sectors到内存的位置

--- include/boot.h
+++ include/boot.h
@@ -3,7 +3,10 @@
 
 #define		BOOT_SIGNATURE 	     0xaa55	
 #define 	BASE_OF_STACK 	     0x7c00
+
+/* 定义从软盘读取sectors到内存的位置，0x7e00=0x7c00+0x200 */
 #define         BASE_OF_READ_SECS    0x7e0
+#define         OFFSET_OF_RADE_SECS  0x0
 
 
 #endif /* BOOT_H */
