d56053539803ac25c7b5749b47e6f6957863d3c0
yangning <yangning1123@gmail.com>
2015-08-15 23:21:29 +0800

    定义从软盘读到内存的基地址

--- include/boot.h
+++ include/boot.h
@@ -1,8 +1,9 @@
 #ifndef BOOT_H
 #define BOOT_H
 
-#define		BOOT_SIGNATURE 	0xaa55	
-#define 	BASE_OF_STACK 	0x7c00
+#define		BOOT_SIGNATURE 	     0xaa55	
+#define 	BASE_OF_STACK 	     0x7c00
+#define         BASE_OF_READ_SECS    0x7e0
 
 
 #endif /* BOOT_H */
