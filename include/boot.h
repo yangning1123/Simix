e767a1f7c784da16fb1044221ab758c36987ab62
yangning <yangning1123@gmail.com>
2015-08-18 00:09:07 +0800

    重新规划内存布局， 将root根目录内容读取到0x7e0:0x0处，将fat entry内容读取到root根目录后的内存位置， 将loader读取到0x9000:0x0处

--- include/boot.h
+++ include/boot.h
@@ -4,9 +4,14 @@
 #define		BOOT_SIGNATURE 	     0xaa55	
 #define 	BASE_OF_STACK 	     0x7c00
 
-/* 定义从软盘读取sectors到内存的位置，0x7e00=0x7c00+0x200 */
-#define         BASE_OF_READ_SECS    0x7e0
-#define         OFFSET_OF_READ_SECS  0x0
+/* 定义从软盘读取根目录到内存的位置，0x7e00=0x7c00+0x200 */
+#define         BASE_OF_ROOT         0x7e0
+#define         OFFSET_OF_ROOT       0x0
+
+
+/* 定以从软盘读取FAT Entry内容到内存位置, 放在root后面，偏移地址须由计算得出 */
+#define         BASE_OF_ENTRY        0x7e0
+     
 
 /* 定义读取loader的内存位置，0x90000=0x9000:0x0 */
 #define         BASE_OF_LOADER       0x9000
