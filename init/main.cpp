7c70b32d0b7dacc0519a05db1f7d823bdffcd6f7
yangning <yangning1123@gmail.com>
2015-11-09 16:12:11 +0800

    8259A中断控制器

--- init/main.cpp
+++ init/main.cpp
@@ -1,5 +1,12 @@
+namespace kernel {
+    void test()
+    {
+	char *video_memory = (char*) 0xb8140;
+	*video_memory = 'X';
+    }
+}
+
 void main()
 {
-    char *video_memory = (char*) 0xb8140;
-    *video_memory = 'X';
+    test();
 }
